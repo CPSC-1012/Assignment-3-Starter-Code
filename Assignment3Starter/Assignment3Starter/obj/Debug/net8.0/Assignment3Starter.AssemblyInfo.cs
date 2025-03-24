b4b791fc21113c7a6f41189c9e5088949123fd8a
sbell <sbell@Nait.ca>
2025-03-23 22:15:42 -0600

    Final

--- Assignment3Starter/Assignment3Starter/obj/Debug/net8.0/Assignment3Starter.AssemblyInfo.cs
+++ Assignment3Starter/Assignment3Starter/obj/Debug/net8.0/Assignment3Starter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment3Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39dbcc43e0a1e57598f0f623f7f24c622fadb85b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment3Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment3Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
